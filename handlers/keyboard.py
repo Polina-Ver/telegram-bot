1d789e7e4128f7ec6f7a49fad52a6d32d7b40dc2
Amelkan <ameliasarma8@gmail.com>
2025-04-30 11:54:37 +0300

    сделала клавиатуру

---
+++ handlers/keyboard.py
@@ -0,0 +1,19 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+button1 = InlineKeyboardButton(
+    text="СИНЯЯ ТАБЛЕТКА",
+    callback_data="life"
+)
+button2 = InlineKeyboardButton(
+    text="КРАСНАЯ ТАБЛЕТКА",
+    callback_data="death"
+)
+
+keyboards = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [button1, button2]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True
+)
+
