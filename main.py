741b04145f6bd5f1900836de3c8ef550e911065b
marus <illmaria257@gmail.com>
2025-04-30 14:29:09 +0300

    исправили ошибки в файле main еще раз

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from config import TOKEN
-from handlers.handlers import router
+from handlers import router as router_handlers
 from handlers import set_my_commands
+from handlers.callbacks import router as router_callbacks
 
 async def main():
     bot = Bot(token=TOKEN)
     dp = Dispatcher()
 
-    # Подключаем роутер к диспетчеру
-    dp.include_router(router)
+    #Определение маршрутизации для диспетчера из handlers
+    dp.include_routers(router_handlers, router_callbacks)
 
     # Установка команд меню
     await set_my_commands(bot)
