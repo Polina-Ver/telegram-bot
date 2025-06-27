e077ac0e98cba2ee4c5d49da0f0d3d264a9bf3e7
Amelkan <ameliasarma8@gmail.com>
2025-06-27 10:47:13 +0300

    Update main.py

--- script/main.py
+++ script/main.py
@@ -3,7 +3,7 @@ from classes import CodewarsLogic
 
 async def main():
     logic = CodewarsLogic()
-    tasks = await logic.parse_and_store("https://www.codewars.com/users/polinakornienko")
+    tasks = await logic.parse_and_store
     print("Parsed tasks:", tasks)
 
 if __name__ == "__main__":
